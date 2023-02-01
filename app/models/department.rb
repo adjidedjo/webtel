e0d670642aebc1daa222b172c36c97c82753251a
aji yulianto <adji.el.djo@gmail.com>
2023-02-01 13:26:08 +0700

    add search options parameter

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,3 +1,7 @@
 class Department < ApplicationRecord
     has_many :employees
+
+    def self.ransackable_attributes(auth_object= nil)
+        %w(name)
+    end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,8 @@ class Employee < ApplicationRecord
     belongs_to :department
 
     validates :name, :department_id, :ext, presence: true
+
+    def self.ransackable_attributes(auth_object= nil)
+        %w(name)
+    end
 end
