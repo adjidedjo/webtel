17b1e7da7e6e04182e96ed44af3c88e3a15e0014
aji yulianto <adji.el.djo@gmail.com>
2023-02-02 14:37:35 +0700

    add alias to department name

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,7 +1,8 @@
 class Department < ApplicationRecord
+    alias_attribute :dept_name, :name
     has_many :employees
 
     def self.ransackable_attributes(auth_object= nil)
-        %w(name)
+        %w(dept_name)
     end
 end
