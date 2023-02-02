a8e9d8eef4cb2faffaffe5c816801fb2d8739b05
aji yulianto <adji.el.djo@gmail.com>
2023-02-02 15:49:00 +0700

    add alias to branch and ransack

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,3 +1,8 @@
 class Branch < ApplicationRecord
+    alias_attribute :branch, :branch_name
     has_many :employees
+
+    def self.ransackable_attributes(auth_object= nil)
+        %w(branch)
+    end
 end
