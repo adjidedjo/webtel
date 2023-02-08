6be5294097c2529cca1c43e49405a4d79c5e0c50
aji yulianto <adji.el.djo@gmail.com>
2023-02-08 14:29:41 +0700

    add jquery and move js to javascripts folder

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,14 @@
 // Entry point for the build script in your package.json
 import { Turbo } from "@hotwired/turbo-rails"
 Turbo.session.drive = false
+import './add_jquery'
 import "./controllers"
 import * as bootstrap from "bootstrap"
 import "./base"
 
 document.addEventListener("turbo:load", () => {
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-  });
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+});
--- app/javascript/base.js
+++ app/javascript/base.js
@@ -1,7 +1,39 @@
+import { Alert, Popover } from "bootstrap";
 
 const passwordInput = document.querySelector("#user_password");
 const eye = document.querySelector("#eye");
-eye.addEventListener("click", function () {
+const clearButton = document.getElementById("reset_button");
+const searchField = document.getElementById("q_c_0_v_0_value");
+
+document.addEventListener("turbolinks:load", function () {
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new Popover(popoverTriggerEl)
+    })
+});
+
+// show hide x button function inside search textfide
+function showHide(param, elm) {
+    if (param.val() !== "") {
+        $(elm).show();
+    } else {
+        $(elm).hide();
+    }
+}
+
+// show or hide x button on search textfield
+$(searchField).keyup(function () {
+    showHide($(this), clearButton);
+}).keyup();
+
+// hide x button when clicked
+$(clearButton).click(function () {
+    searchField.value = "";
+    $(this).hide(); // Trigger the keyup event, thus running the handler on page load
+});
+
+// show hide password field button on login form
+$(eye).click(function () {
     this.classList.toggle("bi-eye-slash-fill")
     const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
     passwordInput.setAttribute("type", type)
