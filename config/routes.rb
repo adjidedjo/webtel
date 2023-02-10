7b43a7a72bfadcbe88be8f2d29ce04fbebcbc99a
aji yulianto <adji.el.djo@gmail.com>
2023-02-10 15:46:40 +0700

    revise routes for users login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   resources :branches
   resources :employees
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users
   resources :departments
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
